1d9e379bb6ca2a2ed97a622e314d322026684d24
AneCode <75664942+AneCode@users.noreply.github.com>
2022-09-29 16:50:47 +0530

    Update TestNightangle.java

--- Lab-Session/29-09-2022/TestNightangle.java
+++ Lab-Session/29-09-2022/TestNightangle.java
@@ -1,5 +1,11 @@
+/* @author:Aneeque Khan
+Program Name:create a class parent with default constructor and void sing method.print something inside the constructor and method
+Create child class which will inherit the property of parent and has methods void speak and void dance.print something inside the two methods.
+Create another class inheritance and object of class and call all the methods.also mention the type of inheritance on the top of the program.
+Date:29-09-2022
+*/
 package corejava;
-
+//Main class for execuation of main method  and creating instance of class of BabyNightangle1 and invoking all  its methods.
 public class TestNightangle {
 
 	public static void main(String[] args) {

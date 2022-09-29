ceb0480b190c17c1ffea6cbc3bf5851e7d223074
AneCode <75664942+AneCode@users.noreply.github.com>
2022-09-29 16:53:35 +0530

    Update Nightingale.java

--- Lab-Session/29-09-2022/Nightingale.java
+++ Lab-Session/29-09-2022/Nightingale.java
@@ -1,3 +1,9 @@
+/* @author:Aneeque Khan
+Program Name:create a class parent with default constructor and void sing method.print something inside the constructor and method
+Create child class which will inherit the property of parent and has methods void speak and void dance.print something inside the two methods.
+Create another class inheritance and object of class and call all the methods.also mention the type of inheritance on the top of the program.
+Date:29-09-2022
+*/
 package corejava;
 
 public class Nightingale{

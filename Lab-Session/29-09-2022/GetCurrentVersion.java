d6b7b89795fa6a91ee8a56237e2d03f58033c5fe
AneCode <75664942+AneCode@users.noreply.github.com>
2022-09-29 16:39:36 +0530

    Add files via upload

---
+++ Lab-Session/29-09-2022/GetCurrentVersion.java
@@ -0,0 +1,13 @@
+package corejava;
+
+public class GetCurrentVersion {
+
+	public GetCurrentVersion() {
+		
+	}
+	public static void main(String n[]) {
+		System.out.println("Java Version is: "+System.getProperty("java.version"));
+	}
+	
+
+}
